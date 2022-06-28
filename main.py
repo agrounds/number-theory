0cb9da139907a4e0ce79d0ad4c366aca4d82ca87
Alex Grounds <agrounds@ojolabs.com>
2022-06-28 15:20:18 -0500

    Add Gauss lemma function

--- main.py
+++ main.py
@@ -34,7 +34,15 @@ def pows(primitives_only=False):
             print(f'{a}^{i} = {m.pow(a, i)}')
 
 
+def gauss_lemma(a, p):
+    for i in range(1, p):
+        b = (i * a) % p
+        if b > (p-1)/2:
+            b -= p
+        print(f'{i} * {a} = {b}')
+
+
 if __name__ == '__main__':
-    m = Modular(29)
-    for x in [16, 24, 7, 25, 23, 20, 1]:
-        print(f'{x}^7 = {m.pow(x, 7)}')
+    m = Modular(7)
+    for i in range(7):
+        print(f'{i}^2 = {m.pow(i, 2)}')
